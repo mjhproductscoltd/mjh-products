8143b4fec9236f4d28c0dde8b302c4c2f06b861b
mjh-products <mjhproductsco.ltd@gmail.com>
2026-06-19 06:15:46 +0300

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,1 +1,24 @@
+let cart = JSON.parse(localStorage.getItem("mjh_cart")) || [];
 
+// ADD TO CART
+function addToCart(id, name, price) {
+
+  cart.push({ id, name, price });
+
+  localStorage.setItem("mjh_cart", JSON.stringify(cart));
+
+  alert(name + " added to cart ✔");
+}
+
+// VIEW CART (console/simple)
+function viewCart() {
+  console.log(cart);
+  alert("Cart items: " + cart.length);
+}
+
+// CLEAR CART
+function clearCart() {
+  cart = [];
+  localStorage.removeItem("mjh_cart");
+  alert("Cart cleared");
+}
