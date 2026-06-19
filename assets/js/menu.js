ab2da7f041794172bfab7b7fa28acde36dcc8dfb
mjhproductscoltd <mjhproductsco.ltd@gmail.com>
2026-06-19 16:50:31 +0300

    Add hamburger menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,7 @@
-function openMenu() {
-  document.getElementById("sideMenu").style.width = "280px";
+function openMenu(){
+  document.getElementById("sideMenu").style.width="280px";
 }
 
-function closeMenu() {
-  document.getElementById("sideMenu").style.width = "0";
+function closeMenu(){
+  document.getElementById("sideMenu").style.width="0";
 }
