8fefc00caa5b4987cae94b2c3f4515578e3d32d1
mjhproductscoltd <mjhproductsco.ltd@gmail.com>
2026-06-22 01:31:25 +0300

    Fix menu fetch path for portable deployment

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/menu_block.html")
+    fetch("menu_block.html")
         .then(res => res.text())
         .then(data => {
             document.body.insertAdjacentHTML("afterbegin", data);
-        });
+        })
+        .catch(err => console.log("Menu load error:", err));
 });
