7a967df6755f13c3a712abe82aff972f2d3bf1e7
mjhproductscoltd <mjhproductsco.ltd@gmail.com>
2026-06-22 01:28:14 +0300

    Modular menu system added via Termux automation

---
+++ assets/js/layout.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/menu_block.html")
+        .then(res => res.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("afterbegin", data);
+        });
+});
